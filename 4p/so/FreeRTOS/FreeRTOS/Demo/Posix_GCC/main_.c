22f06924a95cc41aabd09613e1ae4ed333bd98ba
fernandooal <fernandoalonsops@hotmail.com>
2025-08-22 19:52:04 -0300

    feat: exercícios freeRTOS

--- 4p/so/FreeRTOS/FreeRTOS/Demo/Posix_GCC/main_.c
+++ 4p/so/FreeRTOS/FreeRTOS/Demo/Posix_GCC/main_.c
@@ -2,6 +2,7 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "basic_io.h"
+#include "time.h"
 
 /*
  1 - O protÛtipo da funÁ„o de uma tarefa deve sempre retornar void, ou seja, a tarefa n„o possui retorno.
@@ -9,6 +10,9 @@
 */
 void vTask1(void *pvParameters);
 void vTask2(void* pvParameters);
+void taskNome(void *pvParameters);
+void taskSobrenome(void *pvParameters);
+void taskTempo(void *pvParameters);
 
 /*
  DeniÁ„o da estrutura da funÁ„o
@@ -42,13 +46,60 @@ void vTask2(void* pvParameters)
 void main_(void)
 {
 
-	xTaskCreate(vTask1, "Task 1", 1000, NULL, 1, NULL);
-	xTaskCreate(vTask2, "Task 2", 1000, NULL, 1, NULL);
+	// xTaskCreate(vTask1, "Task 1", 1000, NULL, 1, NULL);
+	// xTaskCreate(vTask2, "Task 2", 1000, NULL, 1, NULL);
+
+	// Prática
+	// xTaskCreate(taskNome, "Task Nome", 1000, NULL, 1, NULL);
+	// xTaskCreate(taskSobrenome, "Task Sobrenome", 1000, NULL, 1, NULL);
+	xTaskCreate(taskTempo, "Task Tempo Local", 1000, NULL, 1, NULL);
 
 	// Inicia o escalonador de tarefas
 	vTaskStartScheduler();
 
 	for (;; );
-//	return 0;
 }
 
+void taskNome(void *pvParameters) {
+	const char* nome = "Fernando ";
+
+	for(;; ){
+		vPrintString(nome);
+		vTaskDelay(500);
+	}
+
+	vTaskDelete(NULL);
+}
+
+void taskSobrenome(void *pvParameters){
+	const char* sobrenome = "Alonso\n";
+
+	for(;; ){
+		vPrintString(sobrenome);
+		vTaskDelay(750);
+	}
+
+	vTaskDelete(NULL);
+}
+
+#include <stdio.h>
+
+void taskTempo(void *pvParameters) {
+    char msg[64];
+    time_t segundos;
+
+    for (;;) {
+        time(&segundos);                          
+        struct tm* currentTime = localtime(&segundos);
+
+        snprintf(msg, sizeof(msg), "Tempo Local: %02d:%02d:%02d\n", 
+                 currentTime->tm_hour, 
+                 currentTime->tm_min, 
+                 currentTime->tm_sec);
+
+        vPrintString(msg);
+        vTaskDelay(1000);
+    }
+
+    vTaskDelete(NULL);
+}
