88ccd261862cee36f68e5d868955ecad9ea09fab
fernandooal <fernandoalonsops@hotmail.com>
2025-08-29 19:58:33 -0300

    feat: pratica freeRTOS so

--- 4p/so/FreeRTOS/FreeRTOS/Demo/Posix_GCC/main_.c
+++ 4p/so/FreeRTOS/FreeRTOS/Demo/Posix_GCC/main_.c
@@ -1,105 +1,71 @@
-
 #include "FreeRTOS.h"
 #include "task.h"
 #include "basic_io.h"
-#include "time.h"
+#include <time.h>
+#include <stdlib.h>
 
-/*
- 1 - O protÛtipo da funÁ„o de uma tarefa deve sempre retornar void, ou seja, a tarefa n„o possui retorno.
- 2 - A funÁ„o de uma tarefa deve receber um par‚metro de ponteiro void. 
-*/
-void vTask1(void *pvParameters);
-void vTask2(void* pvParameters);
-void taskNome(void *pvParameters);
-void taskSobrenome(void *pvParameters);
-void taskTempo(void *pvParameters);
+void monitorarBatimentos(void *param);
+void monitorarOxigenio(void *param);
+void monitorarTemperatura(void *param);
 
-/*
- DeniÁ„o da estrutura da funÁ„o
-*/
-void vTask1(void *pvParameters)
-{
-	const char *msg = "Task 1\n";
+int randint(int min, int max){
+    return rand() % (max - min + 1) + min;
+}
 
-	for (;; )
-	{
-		vPrintString(msg);
-		vTaskDelay(500);
+void monitorarBatimentos(void *param){
+	for (;; ){
+		int bpm = randint(40,150);
+		vPrintStringAndNumber(param, bpm);
+
+		if(bpm < 50 || bpm > 120){
+	 		vPrintString("Atenção! Batimentos cardíacos em estado crítico\n");
+		}
+
+		vTaskDelay(1000);
 	}
-
+	
 	vTaskDelete(NULL);
 }
 
-void vTask2(void* pvParameters)
-{
-	const char* msg = "Task 2\n";
+void monitorarOxigenio(void *param){
+	for (;; ){
+		int saturacao = randint(80,100);
+		vPrintStringAndNumber(param, saturacao);
 
-	for (;; )
-	{
-		vPrintString(msg);
-		vTaskDelay(500);
+		if(saturacao < 90){
+	 		vPrintString("Atenção! Saturação em estado crítico\n");
+		}
+
+		vTaskDelay(1000);
 	}
-
+	
 	vTaskDelete(NULL);
 }
 
-void main_(void)
-{
+void monitorarTemperatura(void *param){
+	for (;; ){
+		int temp = randint(35,38);
+		vPrintStringAndNumber(param, temp);
 
-	// xTaskCreate(vTask1, "Task 1", 1000, NULL, 1, NULL);
-	// xTaskCreate(vTask2, "Task 2", 1000, NULL, 1, NULL);
+		if(temp < 36 || temp > 37.5){
+	 		vPrintString("Atenção! Febre em estado crítico\n");
+		}
 
-	// Prática
-	// xTaskCreate(taskNome, "Task Nome", 1000, NULL, 1, NULL);
-	// xTaskCreate(taskSobrenome, "Task Sobrenome", 1000, NULL, 1, NULL);
-	xTaskCreate(taskTempo, "Task Tempo Local", 1000, NULL, 1, NULL);
+		vTaskDelay(1000);
+	}
+	
+	vTaskDelete(NULL);
+}
 
-	// Inicia o escalonador de tarefas
+void main_(void){
+	srand(time(NULL));
+
+	xTaskCreate(monitorarBatimentos, "Monitoramento de Batimentos", 1000, "Batimentos: ", 1, NULL);
+	xTaskCreate(monitorarOxigenio, "Monitoramento de Oxigênio", 1000, "Saturação: ", 1, NULL);
+	xTaskCreate(monitorarTemperatura, "Monitoramento de Temperatura", 1000, "Temperatura: ", 1, NULL);
+
 	vTaskStartScheduler();
 
 	for (;; );
 }
 
-void taskNome(void *pvParameters) {
-	const char* nome = "Fernando ";
-
-	for(;; ){
-		vPrintString(nome);
-		vTaskDelay(500);
-	}
-
-	vTaskDelete(NULL);
-}
-
-void taskSobrenome(void *pvParameters){
-	const char* sobrenome = "Alonso\n";
-
-	for(;; ){
-		vPrintString(sobrenome);
-		vTaskDelay(750);
-	}
-
-	vTaskDelete(NULL);
-}
-
-#include <stdio.h>
-
-void taskTempo(void *pvParameters) {
-    char msg[64];
-    time_t segundos;
-
-    for (;;) {
-        time(&segundos);                          
-        struct tm* currentTime = localtime(&segundos);
-
-        snprintf(msg, sizeof(msg), "Tempo Local: %02d:%02d:%02d\n", 
-                 currentTime->tm_hour, 
-                 currentTime->tm_min, 
-                 currentTime->tm_sec);
-
-        vPrintString(msg);
-        vTaskDelay(1000);
-    }
-
-    vTaskDelete(NULL);
-}
