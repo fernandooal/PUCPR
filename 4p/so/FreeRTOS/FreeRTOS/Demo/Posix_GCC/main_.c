8f09e3957e766d2987d1dca0be8f7fb64ebae775
fernandooal <fernandoalonsops@hotmail.com>
2025-09-05 19:47:08 -0300

    feat: ra1 entrega so - quadricoptero

--- 4p/so/FreeRTOS/FreeRTOS/Demo/Posix_GCC/main_.c
+++ 4p/so/FreeRTOS/FreeRTOS/Demo/Posix_GCC/main_.c
@@ -1,71 +1,232 @@
+/*
+Curso: Bacharelado em Ciência da Computação
+Disciplina: Sistemas Operacionais Ciberfísicos
+Período: 4
+Turma: U
+Aluno: Fernando Alonso Piroga da Silva
+*/
+
 #include "FreeRTOS.h"
 #include "task.h"
 #include "basic_io.h"
+#include <stdio.h>
+#include <string.h>
 #include <time.h>
 #include <stdlib.h>
 
-void monitorarBatimentos(void *param);
-void monitorarOxigenio(void *param);
-void monitorarTemperatura(void *param);
+void controladorManobras(void *param);
+void arfagem(void *param);
+void guinada(void *param);
+void rolagem(void *param);
+char *printMotores();
 
-int randint(int min, int max){
-    return rand() % (max - min + 1) + min;
-}
+static int motor0, motor1, motor2, motor3;
 
-void monitorarBatimentos(void *param){
-	for (;;){
-		int bpm = randint(40,150);
-		vPrintStringAndNumber(param, bpm);
-
-		if(bpm < 50 || bpm > 120){
-	 		vPrintString("Atenção! Batimentos cardíacos em estado crítico\n");
-		}
-
-		vTaskDelay(1000);
-	}
-	
-	vTaskDelete(NULL);
-}
-
-void monitorarOxigenio(void *param){
-	for (;;){
-		int saturacao = randint(80,100);
-		vPrintStringAndNumber(param, saturacao);
-
-		if(saturacao < 90){
-	 		vPrintString("Atenção! Saturação em estado crítico\n");
-		}
-
-		vTaskDelay(1000);
-	}
-	
-	vTaskDelete(NULL);
-}
-
-void monitorarTemperatura(void *param){
-	for (;;){
-		int temp = randint(35,38);
-		vPrintStringAndNumber(param, temp);
-
-		if(temp < 36 || temp > 37){
-	 		vPrintString("Atenção! Febre em estado crítico\n");
-		}
-
-		vTaskDelay(1000);
-	}
-	
-	vTaskDelete(NULL);
-}
+#define VALOR_INICIAL_MOTORES 5000
 
 void main_(void){
+    motor0 = VALOR_INICIAL_MOTORES;
+    motor1 = VALOR_INICIAL_MOTORES;
+    motor2 = VALOR_INICIAL_MOTORES;
+    motor3 = VALOR_INICIAL_MOTORES;
+
 	srand(time(NULL));
 
-	xTaskCreate(monitorarBatimentos, "Monitoramento de Batimentos", 1000, "Batimentos: ", 1, NULL);
-	xTaskCreate(monitorarOxigenio, "Monitoramento de Oxigênio", 1000, "Saturação: ", 1, NULL);
-	xTaskCreate(monitorarTemperatura, "Monitoramento de Temperatura", 1000, "Temperatura: ", 1, NULL);
+    xTaskCreate(controladorManobras, "controlador", 1000, NULL, 1, NULL);
 
-	vTaskStartScheduler();
+    vTaskStartScheduler();
 
 	for (;;);
 }
 
+void controladorManobras(void *param){
+    // possíveis parâmetros
+    const char *arfagemOpts[] = {"para frente", "para trás"};
+    const char *guinadaOpts[] = {"horário", "anti-horário"};
+    const char *rolagemOpts[] = {"direita", "esquerda"};
+
+    for(;;){
+        // sorteio um valor aleatorio e pego o resto da divisão por 3
+        // (tenho 3 manobras, então só existem 3 cases)
+        int sorteio = rand() % 3;
+
+        // como cada manobra possui 2 possibilidades de parâmetro, vou pegar o resto do mesmo 
+        // sorteio por 2 (0 ou 1) para achar o indice do parametro
+        switch(sorteio){
+            case 0: {
+                int i = sorteio % 2;
+                xTaskCreate(arfagem, "arfagem", 1000, (void*) arfagemOpts[i], 1, NULL);
+                break;
+            }
+            case 1: {
+                int i = sorteio % 2;
+                xTaskCreate(guinada, "guinada", 1000, (void*) guinadaOpts[i], 1, NULL);
+                break;
+            }
+            case 2: {
+                int i = sorteio % 2;
+                xTaskCreate(rolagem, "rolagem", 1000, (void*) rolagemOpts[i], 1, NULL);
+                break;
+            }
+        }
+
+        vTaskDelay(10);
+    }
+}
+
+
+char *printMotores(){
+    static char msg[64];
+
+    // montando a string com os valores dos motores
+    snprintf(msg, sizeof(msg), "Motor 0: %d\nMotor 1: %d\nMotor 2: %d\nMotor3: %d\n", motor0, motor1, motor2, motor3);
+
+    return msg;
+}
+
+void arfagem(void *param){
+    // salvo os valores originais do motor
+    int temp0 = motor0;
+    int temp1 = motor1;
+    int temp2 = motor2;
+    int temp3 = motor3;
+
+    if(strcmp(param, "para frente") == 0){
+        // aumentar velocidade dos motores 2 e 3
+        motor2 *= 25;
+        motor3 *= 25;
+
+        // diminuir velocidade dos outros para empuxo constante
+        motor0 /= 25;
+        motor1 /= 25;
+    } else{
+        // aumentar velocidade dos motores 0 e 1
+        motor0 *= 25;
+        motor1 *= 25;
+
+        // diminuir velocidade dos outros para empuxo constante
+        motor2 /= 25;
+        motor3 /= 25;
+    }
+
+    // print do sistema
+    vPrintString("==================\n");
+    vPrintString("[MANOBRA] ARFAGEM\n");
+
+    vPrintString("[AÇÃO] ");
+    vPrintString(param);
+
+    vPrintString("\n[MOTORES]\n");
+    char *msg = printMotores();
+    vPrintString(msg);
+
+    vPrintString("==================\n");
+
+    // volto os valores dos motores ao original
+    motor0 = temp0;
+    motor1 = temp1;
+    motor2 = temp2;
+    motor3 = temp3;
+
+    vTaskDelay(40);
+
+    vTaskDelete(NULL);
+}
+
+void guinada(void *param){
+    // salvo os valores originais do motor
+    int temp0 = motor0;
+    int temp1 = motor1;
+    int temp2 = motor2;
+    int temp3 = motor3;
+
+    if(strcmp(param, "horário") == 0){
+        // aumentar velocidade dos motores 0 e 2
+        motor0 *= 100;
+        motor2 *= 100;
+
+        // diminuir velocidade dos outros para empuxo constante
+        motor1 /= 100;
+        motor3 /= 100;
+    } else{
+        // aumentar velocidade dos motores 1 e 3
+        motor1 *= 100;
+        motor3 *= 100;
+
+        // diminuir velocidade dos outros para empuxo constante
+        motor0 /= 100;
+        motor2 /= 100;
+    }
+
+    // print do sistema
+    vPrintString("==================\n");
+    vPrintString("[MANOBRA] GUINADA\n");
+
+    vPrintString("[AÇÃO] ");
+    vPrintString(param);
+
+    vPrintString("\n[MOTORES]\n");
+    char *msg = printMotores();
+    vPrintString(msg);
+
+    vPrintString("==================\n");
+
+    // volto os valores dos motores ao original
+    motor0 = temp0;
+    motor1 = temp1;
+    motor2 = temp2;
+    motor3 = temp3;
+
+    vTaskDelay(10);
+    
+    vTaskDelete(NULL);
+}
+
+void rolagem(void *param){
+    // salvo os valores originais do motor
+    int temp0 = motor0;
+    int temp1 = motor1;
+    int temp2 = motor2;
+    int temp3 = motor3;
+
+    if(strcmp(param, "direita") == 0){
+        // aumentar velocidade dos motores 0 e 3
+        motor0 *= 50;
+        motor3 *= 50;
+
+        // diminuir velocidade dos outros para empuxo constante
+        motor1 /= 50;
+        motor2 /= 50;
+    } else{
+        // aumentar velocidade dos motores 1 e 2
+        motor1 *= 50;
+        motor2 *= 50;
+
+        // diminuir velocidade dos outros para empuxo constante
+        motor0 /= 50;
+        motor3 /= 50;
+    }
+
+    // print do sistema
+    vPrintString("==================\n");
+    vPrintString("[MANOBRA] ROLAGEM\n");
+
+    vPrintString("[AÇÃO] ");
+    vPrintString(param);
+
+    vPrintString("\n[MOTORES]\n");
+    char *msg = printMotores();
+    vPrintString(msg);
+
+    vPrintString("==================\n");
+
+    // volto os valores dos motores ao original
+    motor0 = temp0;
+    motor1 = temp1;
+    motor2 = temp2;
+    motor3 = temp3;
+
+    vTaskDelay(20);
+
+    vTaskDelete(NULL);
+}
